748cd3447665521161e9b9e2768b0c03ab5a3811
DUZLAIP <kirillromanov@inbox.ru>
2025-01-16 01:31:50 +0300

    Add files via upload

---
+++ date_changer.py
@@ -0,0 +1,12 @@
+username = 'Кирилл'
+title = 'Домашняя работа'
+content = 'Модуль 1'
+status = 'в работе'
+create_date = 'Дата начала: 12.01.2025'
+issue_date = 'Дата окончания: 17.01.2025'
+print(username)
+print(title )
+print(content)
+print(status)
+print(create_date[0:18])
+print(issue_date[0:21])
