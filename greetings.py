a8f06aab64522060dbe5e445797602ef7e5780df
kirill <kirillromanov@inbox.ru>
2025-01-16 01:13:37 +0300

    Task 1

---
+++ greetings.py
@@ -0,0 +1,12 @@
+username = 'Имя пользователя: Кирилл '
+title = 'Заголовок заметки: Домашняя работа'
+content = 'Описание заметки: Блок 1'
+status = 'Статус заметки: в работе'
+created_data = 'Дата создания заметки д.м.г.: 12.01.2025'
+issue_date = 'Дата истечения заметки д.м.г.: 17.01.2025'
+print(username)
+print(title)
+print(content)
+print(status)
+print(created_data)
+print(issue_date)
